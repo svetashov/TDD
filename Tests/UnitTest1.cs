7fde12535cec135852644fb6aa336a0037e1fd4f
Artyom Svetashov <arsvetashov@gmail.com>
2021-12-05 21:53:42 +0300

    Added test for last used element deleting

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -27,5 +27,27 @@ namespace Tests
             Assert.AreEqual(2, root4);
             Assert.AreEqual(1, count);
         }
+
+        [TestMethod]
+        public void FullCache_Add_RemovedFirstAdded()
+        {
+            ILruCache<int, int> squareRoots = new LruCache<int, int>(3);
+            squareRoots.Add(1, 1);
+            squareRoots.Add(4, 2);
+            squareRoots.Add(9, 3);
+
+            squareRoots.Add(16, 4);
+            int root16 = squareRoots[16];
+            int root9 = squareRoots[9];
+            int root4 = squareRoots[4];
+            int count = squareRoots.Count();
+            bool contains1 = squareRoots.Contains(1);
+
+            Assert.AreEqual(4, root16);
+            Assert.AreEqual(3, root9);
+            Assert.AreEqual(2, root4);
+            Assert.AreEqual(3, count);
+            Assert.IsFalse(contains1);
+        }
     }
 }
